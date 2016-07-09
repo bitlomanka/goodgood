ca42bf711f2286b42ca0ddd3978509a15921d64a
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-07-09 22:34:00 +0300

    header + slider

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function(){
     
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        singleItem:true,
+        itemsTabletSmall:false,
+        navigation: true,
+        navigationText: false,
+        slideSpeed: 400,
+        autoPlay: 4000
+    });
     
 });
 
