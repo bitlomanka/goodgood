cbe8f0cc9c7377b008e10d5d2f34c3c40f6802fe
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-07-11 22:21:59 +0300

    footer + responsive

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -11,6 +11,32 @@ $(document).ready(function(){
         autoPlay: 4000
     });
     
+    $('.reviews__link').on("click", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        
+        var item = $(this).closest('.reviews-avatar__item'),
+            contentItem = $('.reviews__item'),
+            itemPosition = item.index();
+        
+        contentItem.eq(itemPosition)
+        .addClass('reviews_active')
+        .siblings().removeClass('reviews_active');
+        item.addClass('reviews_active')
+        .siblings().removeClass('reviews_active');
+    });
+    
+    $('.mobile-navigation').on("click", function(e){
+        e.stopPropagation();
+        var menu = $('.navigation__list');
+        
+        if(menu.is(":hidden")){
+            menu.slideDown(400);
+        }else{
+            menu.slideUp(400);
+        }
+    });
+    
 });
 
 
